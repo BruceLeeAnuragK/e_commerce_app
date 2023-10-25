4ffdc865f09e5719c9694ffa4dbb1f2720c11d82
BruceLeeK <kanadeanurag87@gmail.com>
2023-10-25 09:19:57 +0530

    25/10/2023 09:19

--- lib/view/screen/login_screen.dart
+++ lib/view/screen/login_screen.dart
@@ -24,6 +24,7 @@ class _LoginPageState extends State<LoginPage> {
     return Scaffold(
       backgroundColor: Colors.red,
       body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
         children: [
           Row(
             mainAxisAlignment: MainAxisAlignment.center,
@@ -52,6 +53,7 @@ class _LoginPageState extends State<LoginPage> {
           Form(
             key: loginKey,
             child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
               children: [
                 Padding(
                   padding: const EdgeInsets.all(15),
@@ -70,18 +72,18 @@ class _LoginPageState extends State<LoginPage> {
                     decoration: InputDecoration(
                       enabledBorder: OutlineInputBorder(
                         borderSide: BorderSide(
-                          color: Colors.blue.shade900,
+                          color: Colors.white,
                         ),
                       ),
                       prefixIcon: Icon(
                         Icons.person,
-                        color: Colors.blue.shade900,
+                        color: Colors.white,
                       ),
-                      suffixIconColor: Colors.blue,
+                      suffixIconColor: Colors.white,
                       border: OutlineInputBorder(
                         borderRadius: BorderRadius.circular(10),
                         borderSide: BorderSide(
-                          color: Colors.blue,
+                          color: Colors.white,
                           width: 3,
                         ),
                       ),
@@ -104,18 +106,18 @@ class _LoginPageState extends State<LoginPage> {
                     decoration: InputDecoration(
                       enabledBorder: OutlineInputBorder(
                         borderSide: BorderSide(
-                          color: Colors.blue.shade900,
+                          color: Colors.white,
                         ),
                       ),
                       prefixIcon: Icon(
                         Icons.email,
-                        color: Colors.blue.shade900,
+                        color: Colors.white,
                       ),
-                      suffixIconColor: Colors.blue,
+                      suffixIconColor: Colors.white,
                       border: OutlineInputBorder(
                         borderRadius: BorderRadius.circular(10),
                         borderSide: BorderSide(
-                          color: Colors.blue,
+                          color: Colors.white,
                           width: 3,
                         ),
                       ),
@@ -125,22 +127,42 @@ class _LoginPageState extends State<LoginPage> {
               ],
             ),
           ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                "If you are already Login then,",
+                style: TextStyle(
+                  color: Colors.white,
+                ),
+              ),
+              MaterialButton(
+                onPressed: () {},
+                child: Text(
+                  "Register",
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              )
+            ],
+          ),
           MaterialButton(
             height: 60,
             shape: OutlineInputBorder(
               borderRadius: BorderRadius.circular(20),
               borderSide: BorderSide(
-                color: Colors.blue.shade900,
+                color: Colors.white,
               ),
             ),
-            color: Colors.blue.shade900,
+            color: Colors.black,
             onPressed: () async {
               await FireStoreHelper.storeHelper.addUser(
-                userModel: UserModel(
-                  username: usernameController.text,
-                  email: emailController.text,
-                ),
-              );
+                  userModel: UserModel(
+                      username: usernameController.text,
+                      email: emailController.text));
               Get.toNamed("/HomePage");
               if (loginKey.currentState!.validate()) {
                 controller.OnSubmit(
@@ -152,7 +174,7 @@ class _LoginPageState extends State<LoginPage> {
               }
             },
             child: Text(
-              "Submit",
+              "Login",
               style: TextStyle(
                 color: Colors.white,
                 fontSize: 20,
@@ -160,12 +182,41 @@ class _LoginPageState extends State<LoginPage> {
               ),
             ),
           ),
-          // Obx(
-          //   () => Text("name : ${controller.username.value}"),
-          // ),
-          // Obx(
-          //   () => Text("last : ${controller.email.value}"),
-          // ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                GestureDetector(
+                  onTap: () {},
+                  child: Container(
+                    height: 50,
+                    width: 50,
+                    child: Image.network(
+                        "https://lh3.googleusercontent.com/COxitqgJr1sJnIDe8-jiKhxDx1FrYbtRHKJ9z_hELisAlapwE9LUPh6fcXIfb5vwpbMl4xl9H9TRFPc5NOO8Sb3VSgIBrfRYvW6cUA"),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(50),
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+                GestureDetector(
+                  onTap: () {},
+                  child: Container(
+                    height: 50,
+                    width: 50,
+                    child: Image.network(
+                      "https://w7.pngwing.com/pngs/339/149/png-transparent-incognito-hd-logo.png",
+                    ),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(50),
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
           Text("User Name : ${LoginController.storage.read("userName") ?? ""}"),
           Text(" Email : ${LoginController.storage.read("email") ?? ""}"),
         ],
